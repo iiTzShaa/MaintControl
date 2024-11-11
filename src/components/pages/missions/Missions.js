59a726c4abe73e85dd8188eb756022971f0fe781
Sharon Shimon <119872535+iiTzShaa@users.noreply.github.com>
2024-11-11 19:53:09 +0200

    mission status display and UI improvements

--- src/components/pages/missions/Missions.js
+++ src/components/pages/missions/Missions.js
@@ -1314,22 +1314,54 @@
 
 // export default Missions;
 
-
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import axios from 'axios';
 import Fab from '@mui/material/Fab';
 import AddIcon from '@mui/icons-material/Add';
-import DeleteIcon from '@mui/icons-material/Delete';
+import Button from '@mui/material/Button';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
 import Checkbox from '@mui/material/Checkbox';
+import CheckBoxIcon from '@mui/icons-material/CheckBox'; 
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank'; 
 import Mission from './Mission';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import LocationCityIcon from '@mui/icons-material/LocationCity';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import TextRotateVerticalIcon from '@mui/icons-material/TextRotateVertical';
+import PriorityHighIcon from '@mui/icons-material/PriorityHigh';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+import CheckBoxOutlineBlankOutlinedIcon from '@mui/icons-material/CheckBoxOutlineBlankOutlined';
 import './Missions.css';
+import axios from 'axios';
 
 const Missions = () => {
   const [missions, setMissions] = useState([]);
   const [selectedMissions, setSelectedMissions] = useState([]);
+  const [openDialog, setOpenDialog] = useState(false);
+  const [openSuccess, setOpenSuccess] = useState(false);
+  const [sortingOrderDate, setSortingOrderDate] = useState('asc');
+  const [sortingOrderAlph, setSortingOrderAlph] = useState('asc');
+  const [sortingOrderCity, setSortingOrderCity] = useState('asc');
+  const [sortingOrderPri, setSortingOrderPri] = useState('asc');
   const [searchCity, setSearchCity] = useState('');
+  const [filter, setFilter] = useState('created_date');
+  const [username, setUsername] = useState('');
   const navigate = useNavigate();
+
+  const handleFilter = (event, newFilter) => {
+    if (newFilter !== null) {
+      setFilter(newFilter);
+    }
+  };
 
   useEffect(() => {
     const fetchMissions = async () => {
@@ -1343,6 +1375,11 @@ const Missions = () => {
     fetchMissions();
   }, []);
 
+  useEffect(() => {
+    const storedUsername = localStorage.getItem('username');
+    if (storedUsername) setUsername(storedUsername);
+  }, []);
+  
   const handleSelectMission = (missionId) => {
     setSelectedMissions((prevSelected) =>
       prevSelected.includes(missionId)
@@ -1350,6 +1387,11 @@ const Missions = () => {
         : [...prevSelected, missionId]
     );
   };
+
+  const sortingHandlerDate = () => setSortingOrderDate(sortingOrderDate === 'asc' ? 'desc' : 'asc');
+  const sortingHandlerAlph = () => setSortingOrderAlph(sortingOrderAlph === 'asc' ? 'desc' : 'asc');
+  const sortingHandlerCity = () => setSortingOrderCity(sortingOrderCity === 'asc' ? 'desc' : 'asc');
+  const sortingHandlerPri = () => setSortingOrderPri(sortingOrderPri === 'asc' ? 'desc' : 'asc');
 
   const deleteSelectedMissions = async () => {
     try {
@@ -1367,38 +1409,95 @@ const Missions = () => {
     }
   };
 
+
+  let filteredMissions = missions.filter((mission) =>
+    mission.city.toLowerCase().includes(searchCity.toLowerCase())
+  );
+
+  if (filter === 'abc' && sortingOrderAlph === 'asc') {
+    filteredMissions = filteredMissions.sort((a, b) => a.title.localeCompare(b.title));
+  } else if (filter === 'abc' && sortingOrderAlph === 'desc') {
+    filteredMissions = filteredMissions.sort((a, b) => b.title.localeCompare(a.title));
+  }
+
+  if (filter === 'city' && sortingOrderCity === 'asc') {
+    filteredMissions = filteredMissions.sort((a, b) => a.city.localeCompare(b.city));
+  } else if (filter === 'city' && sortingOrderCity === 'desc') {
+    filteredMissions = filteredMissions.sort((a, b) => b.city.localeCompare(a.city));
+  }
+
+  if (filter === 'created_date' && sortingOrderDate === 'asc') {
+    filteredMissions = filteredMissions.sort((a, b) => new Date(a.created_date) - new Date(b.created_date));
+  } else if (filter === 'created_date' && sortingOrderDate === 'desc') {
+    filteredMissions = filteredMissions.sort((a, b) => new Date(b.created_date) - new Date(a.created_date));
+  }
+
+  const priorityValues = {
+    High: 3,
+    Medium: 2,
+    Low: 1,
+  };
+
+  if (filter === 'priority') {
+    filteredMissions = filteredMissions.sort((a, b) => {
+      return sortingOrderPri === 'asc'
+        ? priorityValues[a.priority] - priorityValues[b.priority]
+        : priorityValues[b.priority] - priorityValues[a.priority];
+    });
+  }
   const handleEditClick = (missionId) => {
     navigate(`/missions/edit/${missionId}`);
   };
 
-  const filteredMissions = missions.filter((mission) =>
+  const filteredMissionsSerch = missions.filter((mission) =>
     mission.city.toLowerCase().includes(searchCity.toLowerCase())
   );
 
   return (
     <div className="MissionsTableBox">
       <div className="MissionsBtnsBox">
+      <div className="Welcome">Welcome {username}, Total missions: {missions.length}</div>
         <input
           type="text"
           placeholder="Search by city"
           value={searchCity}
           onChange={(e) => setSearchCity(e.target.value)}
-          className="searchBar"
+          className="searchBarByCity"
         />
-        <Link to="/missions/newMission">
-          <Fab size="small" color="primary" aria-label="add">
-            <AddIcon />
-          </Fab>
-        </Link>
-        <Fab
-          size="small"
-          color="secondary"
-          aria-label="delete"
-          onClick={deleteSelectedMissions}
-          disabled={selectedMissions.length === 0}
-        >
-          <DeleteIcon />
-        </Fab>
+        <div className="actionsContainer">
+  <Link to="/missions/newMission">
+    <Fab size="small" color="primary" aria-label="add">
+      <AddIcon />
+    </Fab>
+  </Link>
+  <Fab
+    size="small"
+    aria-label="delete"
+    onClick={deleteSelectedMissions}
+    disabled={selectedMissions.length === 0}
+  >
+    <DeleteIcon />
+  </Fab>
+  <ToggleButtonGroup
+    value={filter}
+    exclusive
+    onChange={handleFilter}
+    aria-label="set filter"
+  >
+    <ToggleButton value="created_date" onClick={sortingHandlerDate}>
+      <CalendarMonthIcon />
+    </ToggleButton>
+    <ToggleButton value="abc" onClick={sortingHandlerAlph}>
+      <TextRotateVerticalIcon />
+    </ToggleButton>
+    <ToggleButton value="city" onClick={sortingHandlerCity}>
+      <LocationCityIcon />
+    </ToggleButton>
+    <ToggleButton value="priority" onClick={sortingHandlerPri}>
+      <PriorityHighIcon />
+    </ToggleButton>
+  </ToggleButtonGroup>
+</div>
       </div>
       <div className="MissionsTable">
         <ul className="Missions">
@@ -1408,6 +1507,12 @@ const Missions = () => {
                 checked={selectedMissions.includes(mission._id)}
                 onChange={() => handleSelectMission(mission._id)}
                 inputProps={{ 'aria-label': 'Mission Checkbox' }}
+                sx={{
+                  color: 'white', // Unchecked color
+                  '&.Mui-checked': {
+                    color: 'black', // Checked color
+                  },
+                }}
               />
               <Mission 
                 mission={mission}
